67e77260c66605053cb8dcc85f8eaeed8c0508ce
CircleCI <devops@sprucelabs.ai>
2021-09-25 11:57:09 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/events.contract.ts
+++ src/.spruce/events/events.contract.ts
@@ -58,9 +58,11 @@ export default [
 	appointmentsDidBookAppointmentsEventContract_v2021_06_23,
 	calendarCreateCalendarEventTypeEventContract_v2021_05_19,
 	calendarListCalendarEventTypesEventContract_v2021_05_19,
+	mySkill16325605671091589MyCoolEventEventContract_v2021_01_22,
+	mySkill16325607429421721MyCoolEventEventContract_v2021_01_22,
+	heartwoodGenerateUrlEventContract_v2021_02_11,
 	heartwoodDidRegisterSkillViewsEventContract_v2021_02_11,
 	heartwoodGetSkillViewsEventContract_v2021_02_11,
-	heartwoodGenerateUrlEventContract_v2021_02_11,
 	heartwoodRegisterSkillViewsEventContract_v2021_02_11,
 	formsConvertPdfToFormEventContract_v2021_07_02,
 	formsConvertPdfToSchemasEventContract_v2021_07_02,
@@ -71,8 +73,6 @@ export default [
 	formsListCompletedFormsEventContract_v2021_07_02,
 	formsListFormsEventContract_v2021_07_02,
 	formsUpdateCompletedFormEventContract_v2021_07_02,
-	mySkill16325605671091589MyCoolEventEventContract_v2021_01_22,
-	mySkill16325607429421721MyCoolEventEventContract_v2021_01_22,
 	...coreEventContracts,
 ]
 
@@ -84,11 +84,15 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 
 		'calendar.list-calendar-event-types::v2021_05_19': CalendarListCalendarEventTypesEventContract_v2021_05_19['eventSignatures']['calendar.list-calendar-event-types::v2021_05_19']
 
+		'my-skill-1632560567109-1589.my-cool-event::v2021_01_22': MySkill16325605671091589MyCoolEventEventContract_v2021_01_22['eventSignatures']['my-skill-1632560567109-1589.my-cool-event::v2021_01_22']
+
+		'my-skill-1632560742942-1721.my-cool-event::v2021_01_22': MySkill16325607429421721MyCoolEventEventContract_v2021_01_22['eventSignatures']['my-skill-1632560742942-1721.my-cool-event::v2021_01_22']
+
+		'heartwood.generate-url::v2021_02_11': HeartwoodGenerateUrlEventContract_v2021_02_11['eventSignatures']['heartwood.generate-url::v2021_02_11']
+
 		'heartwood.did-register-skill-views::v2021_02_11': HeartwoodDidRegisterSkillViewsEventContract_v2021_02_11['eventSignatures']['heartwood.did-register-skill-views::v2021_02_11']
 
 		'heartwood.get-skill-views::v2021_02_11': HeartwoodGetSkillViewsEventContract_v2021_02_11['eventSignatures']['heartwood.get-skill-views::v2021_02_11']
-
-		'heartwood.generate-url::v2021_02_11': HeartwoodGenerateUrlEventContract_v2021_02_11['eventSignatures']['heartwood.generate-url::v2021_02_11']
 
 		'heartwood.register-skill-views::v2021_02_11': HeartwoodRegisterSkillViewsEventContract_v2021_02_11['eventSignatures']['heartwood.register-skill-views::v2021_02_11']
 
@@ -109,9 +113,5 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'forms.list-forms::v2021_07_02': FormsListFormsEventContract_v2021_07_02['eventSignatures']['forms.list-forms::v2021_07_02']
 
 		'forms.update-completed-form::v2021_07_02': FormsUpdateCompletedFormEventContract_v2021_07_02['eventSignatures']['forms.update-completed-form::v2021_07_02']
-
-		'my-skill-1632560567109-1589.my-cool-event::v2021_01_22': MySkill16325605671091589MyCoolEventEventContract_v2021_01_22['eventSignatures']['my-skill-1632560567109-1589.my-cool-event::v2021_01_22']
-
-		'my-skill-1632560742942-1721.my-cool-event::v2021_01_22': MySkill16325607429421721MyCoolEventEventContract_v2021_01_22['eventSignatures']['my-skill-1632560742942-1721.my-cool-event::v2021_01_22']
 	}
 }
