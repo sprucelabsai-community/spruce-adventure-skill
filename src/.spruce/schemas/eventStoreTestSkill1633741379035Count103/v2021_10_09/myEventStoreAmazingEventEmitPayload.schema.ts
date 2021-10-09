15ee106cf85059721d7589b5d1bd6dd5532bcb50
CircleCI <devops@sprucelabs.ai>
2021-10-09 01:32:31 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

---
+++ src/.spruce/schemas/eventStoreTestSkill1633741379035Count103/v2021_10_09/myEventStoreAmazingEventEmitPayload.schema.ts
@@ -0,0 +1,23 @@
+import { SchemaRegistry } from '@sprucelabs/schema'
+import { SpruceSchemas } from '../../schemas.types'
+
+
+
+const myEventStoreAmazingEventEmitPayloadSchema: SpruceSchemas.EventStoreTestSkill1633741379035Count103.v2021_10_09.MyEventStoreAmazingEventEmitPayloadSchema  = {
+	id: 'myEventStoreAmazingEventEmitPayload',
+	version: 'v2021_10_09',
+	namespace: 'EventStoreTestSkill1633741379035Count103',
+	name: '',
+	    fields: {
+	            /** Update me. */
+	            'aTextField': {
+	                label: 'Update me',
+	                type: 'text',
+	                options: undefined
+	            },
+	    }
+}
+
+SchemaRegistry.getInstance().trackSchema(myEventStoreAmazingEventEmitPayloadSchema)
+
+export default myEventStoreAmazingEventEmitPayloadSchema
