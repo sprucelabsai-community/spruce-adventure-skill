8fc9a25793c9904093556fcd1e141481f4a3e470
CircleCI <devops@sprucelabs.ai>
2021-10-11 23:32:13 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/forms/createForm.v2021_07_02.contract.ts
+++ src/.spruce/events/forms/createForm.v2021_07_02.contract.ts
@@ -1,35 +1,37 @@
-import { buildEventContract } from '@sprucelabs/mercury-types'
-import { buildPermissionContract } from '@sprucelabs/mercury-types'
 import createFormEmitTargetAndPayloadSchema from '#spruce/schemas/forms/v2021_07_02/createFormEmitTargetAndPayload.schema'
 import createFormResponsePayloadSchema from '#spruce/schemas/forms/v2021_07_02/createFormResponsePayload.schema'
+import { buildEventContract } from '@sprucelabs/mercury-types'
+import { buildPermissionContract } from '@sprucelabs/mercury-types'
+
 
 const createFormEventContract = buildEventContract({
-	eventSignatures: {
-		'forms.create-form::v2021_07_02': {
-			isGlobal: true,
-			emitPayloadSchema: createFormEmitTargetAndPayloadSchema,
-			responsePayloadSchema: createFormResponsePayloadSchema,
-			emitPermissionContract: buildPermissionContract({
-				id: 'createFormEmitPermissions',
-				name: 'Create Form',
-				requireAllPermissions: false,
-				permissions: [
-					{
-						id: 'can-create-form',
-						name: 'Can create form',
-						defaults: {
-							loggedIn: {
-								default: true,
-							},
-							anonymous: {
-								default: true,
-							},
-						},
-					},
-				],
-			}),
-		},
-	},
+    eventSignatures: {
+        'forms.create-form::v2021_07_02': {
+            isGlobal: true,
+            emitPayloadSchema: createFormEmitTargetAndPayloadSchema,
+            responsePayloadSchema: createFormResponsePayloadSchema,
+            emitPermissionContract: buildPermissionContract({
+  "id": "createFormEmitPermissions",
+  "name": "Create Form",
+  "requireAllPermissions": false,
+  "permissions": [
+    {
+      "id": "can-create-form",
+      "name": "Can create form",
+      "defaults": {
+        "loggedIn": {
+          "default": true
+        },
+        "anonymous": {
+          "default": true
+        }
+      }
+    }
+  ]
+}),
+            
+        }
+    }
 })
 export default createFormEventContract
 
