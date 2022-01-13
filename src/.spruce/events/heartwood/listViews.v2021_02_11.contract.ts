3c9c77d82b232dfeb9ef03cd838279e71c5b9018
CircleCI <devops@sprucelabs.ai>
2022-01-13 23:24:18 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/heartwood/listViews.v2021_02_11.contract.ts
+++ src/.spruce/events/heartwood/listViews.v2021_02_11.contract.ts
@@ -11,17 +11,31 @@ const listViewsEventContract = buildEventContract({
 			emitPermissionContract: buildPermissionContract({
 				id: 'listViewsEmitPermissions',
 				name: 'List skill views',
+				description: null,
 				requireAllPermissions: false,
 				permissions: [
 					{
 						id: 'can-list-skill-views',
 						name: 'Can list skill views',
+						description: null,
+						requireAllStatuses: false,
 						defaults: {
+							skill: null,
+							owner: null,
+							groupManager: null,
+							manager: null,
+							teammate: null,
+							guest: null,
+							anonymous: null,
 							loggedIn: {
 								default: true,
+								clockedIn: null,
+								clockedOut: null,
+								onPrem: null,
+								offPrem: null,
 							},
 						},
-						requireAllStatuses: false,
+						can: null,
 					},
 				],
 			}),
