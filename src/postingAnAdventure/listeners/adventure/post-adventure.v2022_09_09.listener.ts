5970b0a838482ba0023793e46f8b29667a2e900a
Taylor Romero <me@taylorrome.ro>
2022-09-09 11:24:03 -0600

    patch: stub listener

---
+++ src/postingAnAdventure/listeners/adventure/post-adventure.v2022_09_09.listener.ts
@@ -0,0 +1,21 @@
+import { SkillEventContract } from '@sprucelabs/mercury-types'
+import {
+	SpruceEvent,
+	SpruceEventResponse,
+} from '@sprucelabs/spruce-event-utils'
+import { SpruceSchemas } from '#spruce/schemas/schemas.types'
+import EventFaker from '../../../__tests__/support/EventFaker'
+
+export default async (
+	_event: SpruceEvent<SkillEventContract, EmitPayload>
+): SpruceEventResponse<ResponsePayload> => {
+	return {
+		adventure: new EventFaker().generateAdventureValues(),
+	}
+}
+
+type EmitPayload =
+	SpruceSchemas.Adventure.v2022_09_09.PostAdventureEmitTargetAndPayload
+
+type ResponsePayload =
+	SpruceSchemas.Adventure.v2022_09_09.PostAdventureResponsePayload
