9ddc29018d224a2d9a5f101c7bf543e8730d60c2
Taylor Romero <me@taylorrome.ro>
2022-09-08 22:04:18 -0600

    patch: adventure progress

--- src/__tests__/behavioral/root/PostCard.test.ts
+++ src/__tests__/behavioral/root/PostCard.test.ts
@@ -1,8 +1,15 @@
-import { formAssert } from '@sprucelabs/heartwood-view-controllers'
+import {
+	formAssert,
+	interactor,
+	vcAssert,
+} from '@sprucelabs/heartwood-view-controllers'
 import { fake } from '@sprucelabs/spruce-test-fixtures'
 import { AbstractSpruceFixtureTest } from '@sprucelabs/spruce-test-fixtures'
 import { assert, test } from '@sprucelabs/test'
-import PostCardViewController from '../../../viewControllers/PostCard.vc'
+import { generateId } from '@sprucelabs/test-utils'
+import PostCardViewController, {
+	Adventure,
+} from '../../../viewControllers/PostCard.vc'
 
 @fake.login()
 export default class PostCardTest extends AbstractSpruceFixtureTest {
@@ -20,13 +27,77 @@ export default class PostCardTest extends AbstractSpruceFixtureTest {
 	}
 
 	@test()
-	protected static async postFormRendersExpectedFields() {
-		formAssert.formRendersFields(this.formVc, ['what', 'when', 'where'])
+	protected static async rendersExpectedFields() {
+		this.assertDoesNotRenderSubmitControls()
+
+		this.assertRendersFields(['what'])
+		this.assertDoesNotRenderFields(['when', 'where'])
+
+		await this.setToRandomValue('what')
+
+		this.assertRendersFields(['what', 'when'])
+		this.assertDoesNotRenderField('where')
+
+		await this.setToRandomValue('when')
+
+		this.assertRendersFields(['where'])
+		this.assertDoesNotRenderSubmitControls()
+
+		await this.setRandomAddress()
+
+		this.assertRendersSubmitControls()
 	}
 
 	@test()
 	protected static async doesNotRenderCancelButton() {
 		assert.isFalse(this.formVc.getShouldShowCancelButton())
+	}
+
+	@test()
+	protected static async submittingFormRendersConfirm() {
+		await vcAssert.assertRendersConfirm(this.vc, () =>
+			interactor.submitForm(this.formVc)
+		)
+	}
+
+	private static assertDoesNotRenderFields(names: AdventureKey[]) {
+		names.forEach((i) => this.assertDoesNotRenderField(i))
+	}
+
+	private static async setRandomAddress() {
+		await this.formVc.setValue('where', {
+			city: generateId(),
+			country: generateId(),
+			province: generateId(),
+			street1: generateId(),
+			zip: generateId(),
+		})
+	}
+
+	private static assertDoesNotRenderSubmitControls() {
+		assert.isFalse(
+			this.formVc.getShouldShowSubmitControls(),
+			'should not render submit countrols'
+		)
+	}
+
+	private static assertRendersSubmitControls() {
+		assert.isTrue(
+			this.formVc.getShouldShowSubmitControls(),
+			'should render submit countrols'
+		)
+	}
+
+	private static assertDoesNotRenderField(name: AdventureKey) {
+		formAssert.formDoesNotRenderField(this.formVc, name)
+	}
+
+	private static assertRendersFields(expected: AdventureKey[]) {
+		formAssert.formRendersFields(this.formVc, expected)
+	}
+
+	private static async setToRandomValue(key: 'what' | 'when') {
+		await this.formVc.setValue(key, generateId())
 	}
 
 	private static get formVc() {
@@ -39,3 +110,5 @@ class SpyPostCard extends PostCardViewController {
 		return this.formVc
 	}
 }
+
+type AdventureKey = keyof Adventure
--- src/viewControllers/PostCard.vc.ts
+++ src/viewControllers/PostCard.vc.ts
@@ -4,15 +4,19 @@ import {
 	Card,
 	CardViewController,
 	FormViewController,
+	SpruceSchemas,
 	ViewControllerOptions,
 } from '@sprucelabs/heartwood-view-controllers'
-import { buildSchema } from '@sprucelabs/schema'
+import { buildSchema, SchemaValues } from '@sprucelabs/schema'
 import { randomUtil } from '@sprucelabs/spruce-skill-utils'
+
+type FormSection =
+	SpruceSchemas.HeartwoodViewControllers.v2021_02_11.FormSection<AdventureSchema>
 
 export default class PostCardViewController extends AbstractViewController<Card> {
 	public static id = 'post-card'
 	private cardVc: CardViewController
-	protected formVc: FormViewController<PostFormSchema>
+	protected formVc: FormViewController<AdventureSchema>
 
 	public constructor(options: ViewControllerOptions) {
 		super(options)
@@ -46,13 +50,49 @@ export default class PostCardViewController extends AbstractViewController<Card>
 			buildForm({
 				schema: formSchema,
 				shouldShowCancelButton: false,
-				sections: [
-					{
-						fields: [{ name: 'what', renderAs: 'textarea' }, 'when', 'where'],
-					},
-				],
+				shouldShowSubmitControls: false,
+				onSubmit: this.handleSubmit.bind(this),
+				onChange: this.handleChange.bind(this),
+				sections: [this.renderSections()],
 			})
 		)
+	}
+
+	private renderSections(): FormSection {
+		const fields: FormSection['fields'] = [
+			{ name: 'what', renderAs: 'textarea' },
+		]
+
+		if (this.formVc) {
+			if (this.hasWhatValue()) {
+				fields.push('when')
+			}
+			if (this.formVc.getValue('when')) {
+				fields.push('where')
+			}
+
+			if (this.formVc.getValue('where')) {
+				this.formVc.showSubmitControls()
+			}
+		}
+
+		return {
+			fields,
+		}
+	}
+
+	private hasWhatValue() {
+		return !!this.formVc.getValue('what')
+	}
+
+	private async handleChange() {
+		this.formVc.setSection(0, this.renderSections())
+	}
+
+	public async handleSubmit() {
+		await this.confirm({
+			title: 'You ready to rock!?? 🎸',
+		})
 	}
 
 	public render() {
@@ -76,10 +116,11 @@ const formSchema = buildSchema({
 		},
 		where: {
 			type: 'address',
-			label: 'Where is it going down?',
+			label: 'Where are you going to do it?',
 			isRequired: true,
 		},
 	},
 })
 
-type PostFormSchema = typeof formSchema
+type AdventureSchema = typeof formSchema
+export type Adventure = SchemaValues<AdventureSchema>
