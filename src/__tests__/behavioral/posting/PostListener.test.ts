387b837d2db56ee7702a94eaba60d3b32afbe70d
Taylor Romero <me@taylorrome.ro>
2022-09-10 16:05:52 -0600

    patch: can post adventure

---
+++ src/__tests__/behavioral/posting/PostListener.test.ts
@@ -0,0 +1,39 @@
+import { fake } from '@sprucelabs/spruce-test-fixtures'
+import { test, assert } from '@sprucelabs/test'
+import AbstractAdventureTest from '../../support/AbstractAdventureTest'
+import generateAdventureValues from '../../support/generateAdventureValues'
+
+@fake.login()
+export default class PostListenerTest extends AbstractAdventureTest {
+	@test()
+	protected static async postingAdventureSavesUnderCurrentPerson() {
+		await this.bootSkill()
+		const adventure = this.generatePostAdventureValues()
+
+		const [{ adventure: created }] =
+			await this.fakedClient.emitAndFlattenResponses(
+				'adventure.post::v2022_09_09',
+				{
+					payload: {
+						adventure,
+					},
+				}
+			)
+
+		const saved = await this.adventures.findOne({})
+		assert.doesInclude(saved, {
+			...adventure,
+			source: { personId: this.fakedPerson.id },
+		})
+		assert.isEqualDeep(created, saved)
+	}
+
+	private static generatePostAdventureValues() {
+		const adventure = generateAdventureValues()
+		//@ts-ignore
+		delete adventure.id
+		//@ts-ignore
+		delete adventure.source
+		return adventure
+	}
+}
--- src/posting/listeners/adventure/post.v2022_09_09.listener.ts
+++ src/posting/listeners/adventure/post.v2022_09_09.listener.ts
@@ -4,13 +4,24 @@ import {
 	SpruceEventResponse,
 } from '@sprucelabs/spruce-event-utils'
 import { SpruceSchemas } from '#spruce/schemas/schemas.types'
-import EventFaker from '../../../__tests__/support/EventFaker'
 
 export default async (
-	_event: SpruceEvent<SkillEventContract, EmitPayload>
+	event: SpruceEvent<SkillEventContract, EmitPayload>
 ): SpruceEventResponse<ResponsePayload> => {
+	const { payload, source, stores } = event
+	const { adventure } = payload
+	const { personId } = source
+
+	const adventures = await stores.getStore('adventures')
+	const created = await adventures.createOne({
+		...adventure,
+		source: {
+			personId: personId!,
+		},
+	})
+
 	return {
-		adventure: new EventFaker().generateAdventureValues(),
+		adventure: created,
 	}
 }
 
