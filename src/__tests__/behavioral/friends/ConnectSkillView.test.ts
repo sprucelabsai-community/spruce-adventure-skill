a7ec1cca303276a43253b449b673878a9b1c8bdf
Taylor Romero <me@taylorrome.ro>
2022-09-12 12:50:50 -0600

    patch: connect skill view

---
+++ src/__tests__/behavioral/friends/ConnectSkillView.test.ts
@@ -0,0 +1,14 @@
+import { fake } from '@sprucelabs/spruce-test-fixtures'
+import { test, assert } from '@sprucelabs/test'
+import ConnectSkillViewController from '../../../skillViewControllers/Connect.svc'
+import AbstractAdventureTest from '../../support/AbstractAdventureTest'
+
+@fake.login()
+export default class AcceptSkillViewTest extends AbstractAdventureTest {
+	private static vc: ConnectSkillViewController
+	@test()
+	protected static async hasConnectSkillView() {
+		this.vc = this.views.Controller('adventure.connect', {})
+		this.views.render(this.vc)
+	}
+}
