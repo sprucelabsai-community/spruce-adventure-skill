5aae9de49eec19354a753322a4f1628209de89e8
CircleCI <devops@sprucelabs.ai>
2022-09-15 12:22:47 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -32,16 +32,16 @@ const listeners: EventFeatureListener[] = [
         callback: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').default,
     },
     {
-        eventName: 'list',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
-    },
-    {
         eventName: 'post',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
         callback: require('../../posting/listeners/adventure/post.v2022_09_09.listener').default,
+    },
+    {
+        eventName: 'list',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
     },
 ]
 
