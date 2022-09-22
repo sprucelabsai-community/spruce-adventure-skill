1530fe95adffc461db1c16c7ba0a5b17307cbd7b
CircleCI <devops@sprucelabs.ai>
2022-09-22 12:26:04 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/errors/errors.types.ts
+++ src/.spruce/errors/errors.types.ts
@@ -29,44 +29,4 @@ export declare namespace SpruceErrors.Adventure {
 
 
 
-export declare namespace SpruceErrors.Adventure {
-
-	
-	export interface AdventureSkillError {
-		
-			/** First Field. */
-			'fieldName1': string
-			/** Second Field. A hint */
-			'fieldName2': number
-	}
-
-	export interface AdventureSkillErrorSchema extends SpruceSchema.Schema {
-		id: 'adventureSkillError',
-		namespace: 'Adventure',
-		name: 'adventure skill error',
-		    fields: {
-		            /** First Field. */
-		            'fieldName1': {
-		                label: 'First Field',
-		                type: 'text',
-		                isRequired: true,
-		                options: undefined
-		            },
-		            /** Second Field. A hint */
-		            'fieldName2': {
-		                label: 'Second Field',
-		                type: 'number',
-		                isRequired: true,
-		                hint: 'A hint',
-		                options: undefined
-		            },
-		    }
-	}
-
-	export type AdventureSkillErrorEntity = SchemaEntity<SpruceErrors.Adventure.AdventureSkillErrorSchema>
-
-}
-
-
-
 
