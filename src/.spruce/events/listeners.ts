ab78edf771302bccec6ce5861123ccab9342fe0a
Taylor Romero <me@taylorrome.ro>
2022-10-13 13:17:06 -0600

    major: upgrade

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -14,10 +14,16 @@ const listeners: EventFeatureListener[] = [
         callback: require('../../listeners/skill/will-boot.v2021_08_03.listener').default,
     },
     {
-        eventName: 'cancel',
+        eventName: 'register-dashboard-cards',
+        eventNamespace: 'people',
+        version: 'v2022_05_29',
+        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
+    },
+    {
+        eventName: 'list',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
-        callback: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').default,
+        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
     },
     {
         eventName: 'accept-connection',
@@ -38,28 +44,22 @@ const listeners: EventFeatureListener[] = [
         callback: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').default,
     },
     {
-        eventName: 'register-dashboard-cards',
-        eventNamespace: 'people',
-        version: 'v2022_05_29',
-        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
-    },
-    {
         eventName: 'post',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
         callback: require('../../posting/listeners/adventure/post.v2022_09_09.listener').default,
     },
     {
-        eventName: 'list',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
-    },
-    {
         eventName: 'rsvp',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
         callback: require('../../rsvping/listeners/adventure/rsvp.v2022_09_09.listener').default,
+    },
+    {
+        eventName: 'cancel',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').default,
     },
 ]
 
