48877d1553f44b1cfd49c48664b2848d86511459
CircleCI <devops@sprucelabs.ai>
2022-12-10 14:50:15 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -3,8 +3,8 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'heartwood.skill-views': [
 			'can-get-skill-views','can-register-skill-views','can-listen-to-did-register-skill-views','can-generate-url','can-get-skill-views','can-list-skill-views','can-get-dashboard-cards','can-get-active-theme','can-manage-organization-themes',
 		]
-		'people.searchEmitPermissions': [
-			'can-search-people',
+		'people.getListenPermissions': [
+			'can-high-five',
 		]
 		'people.getEmitPermissions': [
 			'can-get-person',
@@ -15,8 +15,8 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'people.registerDashboardCardsEmitPermissions': [
 			'can-register-dashboard-cards',
 		]
-		'people.getListenPermissions': [
-			'can-high-five',
+		'people.searchEmitPermissions': [
+			'can-search-people',
 		]
 	}
 }
