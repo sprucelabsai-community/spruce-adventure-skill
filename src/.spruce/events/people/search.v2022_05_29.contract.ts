be0d583bcc098ef740d3e9388eb5a687bb3bf6e9
CircleCI <devops@sprucelabs.ai>
2022-12-13 08:49:50 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/people/search.v2022_05_29.contract.ts
+++ src/.spruce/events/people/search.v2022_05_29.contract.ts
@@ -1,6 +1,5 @@
 import '#spruce/permissions/permissions.types'
 import { buildEventContract } from '@sprucelabs/mercury-types'
-import { buildPermissionContract } from '@sprucelabs/mercury-types'
 import searchEmitTargetAndPayloadSchema from '#spruce/schemas/people/v2022_05_29/searchEmitTargetAndPayload.schema'
 import searchResponsePayloadSchema from '#spruce/schemas/people/v2022_05_29/searchResponsePayload.schema'
 
@@ -8,58 +7,13 @@ const searchEventContract = buildEventContract({
 	eventSignatures: {
 		'people.search::v2022_05_29': {
 			isGlobal: true,
+			emitPermissions: {
+				contractId: 'people.people-contract',
+				permissionIdsAny: ['can-search-people', 'can-search-people-globally'],
+			},
 
 			emitPayloadSchema: searchEmitTargetAndPayloadSchema,
 			responsePayloadSchema: searchResponsePayloadSchema,
-			emitPermissionContract: buildPermissionContract({
-				id: 'searchEmitPermissions',
-				name: 'Search',
-				description: null,
-				requireAllPermissions: false,
-				permissions: [
-					{
-						id: 'can-search-people',
-						name: 'Can search people',
-						description: null,
-						requireAllStatuses: false,
-						defaults: {
-							skill: null,
-							owner: {
-								default: true,
-								clockedIn: null,
-								clockedOut: null,
-								onPrem: null,
-								offPrem: null,
-							},
-							groupManager: {
-								default: true,
-								clockedIn: null,
-								clockedOut: null,
-								onPrem: null,
-								offPrem: null,
-							},
-							manager: {
-								default: true,
-								clockedIn: null,
-								clockedOut: null,
-								onPrem: null,
-								offPrem: null,
-							},
-							teammate: {
-								default: true,
-								clockedIn: null,
-								clockedOut: null,
-								onPrem: null,
-								offPrem: null,
-							},
-							anonymous: null,
-							loggedIn: null,
-							guest: null,
-						},
-						can: null,
-					},
-				],
-			}),
 		},
 	},
 })
