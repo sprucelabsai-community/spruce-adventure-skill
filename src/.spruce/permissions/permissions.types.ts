715588efca9d2296c8e9e45c1aa71f48de3bf238
CircleCI <devops@sprucelabs.ai>
2023-01-28 21:44:32 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -3,14 +3,14 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'people.people-contract': [
 			'can-get-person','can-search-people','can-search-people-globally',
 		]
+		'heartwood.skill-views': [
+			'can-get-skill-views','can-register-skill-views','can-listen-to-did-register-skill-views','can-generate-url','can-get-skill-views','can-list-skill-views','can-get-dashboard-cards','can-get-active-theme','can-manage-organization-themes',
+		]
 		'people.registerDashboardCardsListenPermissions': [
 			'can-register-dashboard-cards',
 		]
 		'people.registerDashboardCardsEmitPermissions': [
 			'can-register-dashboard-cards',
-		]
-		'heartwood.skill-views': [
-			'can-get-skill-views','can-register-skill-views','can-listen-to-did-register-skill-views','can-generate-url','can-get-skill-views','can-list-skill-views','can-get-dashboard-cards','can-get-active-theme','can-manage-organization-themes',
 		]
 	}
 }
