a1e8c65ba00a6c5875027c817d3cf7c8509bbb9b
CircleCI <devops@sprucelabs.ai>
2023-02-02 20:45:56 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -1,15 +1,15 @@
 declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 	interface PermissionContractMap {
-		'people.people-contract': [
-			'can-get-person','can-search-people','can-search-people-globally',
-		]
-		'people.registerDashboardCardsEmitPermissions': [
-			'can-register-dashboard-cards',
-		]
 		'heartwood.skill-views': [
 			'can-get-skill-views','can-register-skill-views','can-listen-to-did-register-skill-views','can-generate-url','can-get-skill-views','can-list-skill-views','can-get-dashboard-cards','can-get-active-theme','can-manage-organization-themes',
 		]
+		'people.people-contract': [
+			'can-get-person','can-search-people','can-search-people-globally',
+		]
 		'people.registerDashboardCardsListenPermissions': [
+			'can-register-dashboard-cards',
+		]
+		'people.registerDashboardCardsEmitPermissions': [
 			'can-register-dashboard-cards',
 		]
 	}
