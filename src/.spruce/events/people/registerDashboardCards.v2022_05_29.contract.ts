9cd7032a39f1a2c0c9eb114ad5ebded37eed88b6
CircleCI <devops@sprucelabs.ai>
2023-02-09 00:56:34 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/people/registerDashboardCards.v2022_05_29.contract.ts
+++ src/.spruce/events/people/registerDashboardCards.v2022_05_29.contract.ts
@@ -1,70 +1,21 @@
 import '#spruce/permissions/permissions.types'
 import { buildEventContract } from '@sprucelabs/mercury-types'
-import { buildPermissionContract } from '@sprucelabs/mercury-types'
 import registerDashboardCardsResponsePayloadSchema from '#spruce/schemas/people/v2022_05_29/registerDashboardCardsResponsePayload.schema'
 
 const registerDashboardCardsEventContract = buildEventContract({
 	eventSignatures: {
 		'people.register-dashboard-cards::v2022_05_29': {
 			isGlobal: true,
+			emitPermissions: {
+				contractId: 'people.people-contract',
+				permissionIdsAny: ['can-see-dashboard-cards'],
+			},
+			listenPermissions: {
+				contractId: 'people.people-contract',
+				permissionIdsAny: ['can-register-dashboard-cards'],
+			},
 
 			responsePayloadSchema: registerDashboardCardsResponsePayloadSchema,
-			emitPermissionContract: buildPermissionContract({
-				id: 'registerDashboardCardsEmitPermissions',
-				name: 'Register dashboard cards',
-				description: null,
-				requireAllPermissions: false,
-				permissions: [
-					{
-						id: 'can-register-dashboard-cards',
-						name: 'Register get registered cards',
-						description: null,
-						requireAllStatuses: null,
-						defaults: {
-							skill: null,
-							owner: null,
-							groupManager: null,
-							manager: null,
-							teammate: null,
-							anonymous: null,
-							loggedIn: {
-								default: true,
-								clockedIn: null,
-								clockedOut: null,
-								onPrem: null,
-								offPrem: null,
-							},
-							guest: null,
-						},
-						can: null,
-					},
-				],
-			}),
-			listenPermissionContract: buildPermissionContract({
-				id: 'registerDashboardCardsListenPermissions',
-				name: 'Register dashboard cards',
-				description: null,
-				requireAllPermissions: false,
-				permissions: [
-					{
-						id: 'can-register-dashboard-cards',
-						name: 'Can register dashboard cards',
-						description: null,
-						requireAllStatuses: false,
-						defaults: {
-							skill: true,
-							owner: null,
-							groupManager: null,
-							manager: null,
-							teammate: null,
-							anonymous: null,
-							loggedIn: null,
-							guest: null,
-						},
-						can: null,
-					},
-				],
-			}),
 		},
 	},
 })
