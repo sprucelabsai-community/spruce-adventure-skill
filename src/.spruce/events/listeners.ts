18b82382ea78a3cbd6d39daf7b9b423bba3f1dbe
CircleCI <devops@sprucelabs.ai>
2024-04-24 01:13:03 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -22,6 +22,17 @@ const listeners: EventFeatureListener[] = [
                 .isGlobal,
     },
     {
+        eventName: 'cancel',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback:
+            require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener')
+                .default,
+        isGlobal:
+            require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener')
+                .isGlobal,
+    },
+    {
         eventName: 'accept-connection',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
@@ -55,28 +66,6 @@ const listeners: EventFeatureListener[] = [
                 .isGlobal,
     },
     {
-        eventName: 'register-dashboard-cards',
-        eventNamespace: 'people',
-        version: 'v2022_05_29',
-        callback:
-            require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener')
-                .default,
-        isGlobal:
-            require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener')
-                .isGlobal,
-    },
-    {
-        eventName: 'cancel',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback:
-            require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener')
-                .default,
-        isGlobal:
-            require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener')
-                .isGlobal,
-    },
-    {
         eventName: 'list',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
@@ -85,6 +74,17 @@ const listeners: EventFeatureListener[] = [
                 .default,
         isGlobal:
             require('../../listing/listeners/adventure/list.v2022_09_09.listener')
+                .isGlobal,
+    },
+    {
+        eventName: 'register-dashboard-cards',
+        eventNamespace: 'people',
+        version: 'v2022_05_29',
+        callback:
+            require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener')
+                .default,
+        isGlobal:
+            require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener')
                 .isGlobal,
     },
     {
