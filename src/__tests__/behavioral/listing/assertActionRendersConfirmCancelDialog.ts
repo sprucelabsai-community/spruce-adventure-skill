44c3fd5582b68257f6f675b679d1580237caac04
Taylor Romero <me@taylorrome.ro>
2024-06-06 09:45:52 -0600

    patch: lint fix

--- src/__tests__/behavioral/listing/assertActionRendersConfirmCancelDialog.ts
+++ src/__tests__/behavioral/listing/assertActionRendersConfirmCancelDialog.ts
@@ -1,17 +1,17 @@
-import { vcAssert } from '@sprucelabs/heartwood-view-controllers';
-import ConfirmCancelCardViewController from '../../../cancelling/ConfirmCancelCard.vc';
-import CurrentAdventureCardViewController from '../../../listing/CurrentAdventureCard.vc';
-import ControlledConfirmCancelCard from './ControlledConfirmCancelCard';
+import { vcAssert } from '@sprucelabs/heartwood-view-controllers'
+import ConfirmCancelCardViewController from '../../../cancelling/ConfirmCancelCard.vc'
+import CurrentAdventureCardViewController from '../../../listing/CurrentAdventureCard.vc'
+import ControlledConfirmCancelCard from './ControlledConfirmCancelCard'
 
 export async function assertActionRendersConfirmCancelDialog(
     vc: CurrentAdventureCardViewController,
     action: () => Promise<void>
 ) {
-    const dlgVc = await vcAssert.assertRendersDialog(vc, action);
+    const dlgVc = await vcAssert.assertRendersDialog(vc, action)
 
     const confirmCancelVc = vcAssert.assertRendersAsInstanceOf(
         dlgVc,
         ConfirmCancelCardViewController
-    ) as ControlledConfirmCancelCard;
-    return { confirmCancelVc, dlgVc };
+    ) as ControlledConfirmCancelCard
+    return { confirmCancelVc, dlgVc }
 }
