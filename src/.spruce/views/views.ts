3929e38847cda40c8c2671a9f64e96253454504b
CircleCI <devops@sprucelabs.ai>
2024-06-14 05:28:18 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -1,5 +1,5 @@
-import ListSkillViewController from '../../listing/List.svc'
 import ConnectSkillViewController from '../../friends/Connect.svc'
+import ListSkillViewController from '../../listing/List.svc'
 import PostSkillViewController from '../../posting/Post.svc'
 import EquipSkillViewController from '../../root/Equip.svc'
 import ProfileSkillViewController from '../../root/Profile.svc'
@@ -8,15 +8,15 @@ import ConfirmCancelCardViewController from '../../cancelling/ConfirmCancelCard.
 import AdventureCardViewController from '../../listing/AdventureCard.vc'
 import BaseAdventureCardViewController from '../../listing/BaseAdventureCard.vc'
 import CurrentAdventureCardViewController from '../../listing/CurrentAdventureCard.vc'
-import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
 import FriendsListToolViewController from '../../friends/FriendsListTool.vc'
+import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
 import PostCardViewController from '../../posting/PostCard.vc'
 
 import '@sprucelabs/heartwood-view-controllers'
 
 const vcs = {
-    ListSkillViewController,
     ConnectSkillViewController,
+    ListSkillViewController,
     PostSkillViewController,
     EquipSkillViewController,
     ProfileSkillViewController,
@@ -25,8 +25,8 @@ const vcs = {
     AdventureCardViewController,
     BaseAdventureCardViewController,
     CurrentAdventureCardViewController,
-    FriendsDashboardCardViewController,
     FriendsListToolViewController,
+    FriendsDashboardCardViewController,
     PostCardViewController,
 }
 
@@ -37,8 +37,8 @@ type LoadOptions<Args extends Record<string,any>[]> = Args[0]['args'] extends Re
 
 declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.types' {
 	interface SkillViewControllerMap {
-		'adventure.list': ListSkillViewController
 		'adventure.connect': ConnectSkillViewController
+		'adventure.list': ListSkillViewController
 		'adventure.post': PostSkillViewController
 		'adventure.equip': EquipSkillViewController
 		'adventure.profile': ProfileSkillViewController
@@ -46,8 +46,8 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 	}
 
 	interface SkillViewControllerArgsMap {
-		'adventure.list': LoadOptions<Parameters<ListSkillViewController['load']>>
 		'adventure.connect': LoadOptions<Parameters<ConnectSkillViewController['load']>>
+		'adventure.list': LoadOptions<Parameters<ListSkillViewController['load']>>
 		'adventure.post': LoadOptions<Parameters<PostSkillViewController['load']>>
 		'adventure.equip': LoadOptions<Parameters<EquipSkillViewController['load']>>
 		'adventure.profile': LoadOptions<Parameters<ProfileSkillViewController['load']>>
@@ -59,11 +59,11 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 		'adventure.adventure-card': AdventureCardViewController
 		'adventure.base-adventure-card': BaseAdventureCardViewController
 		'adventure.current-adventure-card': CurrentAdventureCardViewController
-		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
 		'adventure.friends-list-tool': FriendsListToolViewController
+		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
 		'adventure.post-card': PostCardViewController
-		'adventure.list': ListSkillViewController
 		'adventure.connect': ConnectSkillViewController
+		'adventure.list': ListSkillViewController
 		'adventure.post': PostSkillViewController
 		'adventure.equip': EquipSkillViewController
 		'adventure.profile': ProfileSkillViewController
@@ -75,8 +75,8 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 		'adventure.adventure-card': ConstructorParameters<typeof AdventureCardViewController>[0]
 		'adventure.base-adventure-card': ConstructorParameters<typeof BaseAdventureCardViewController>[0]
 		'adventure.current-adventure-card': ConstructorParameters<typeof CurrentAdventureCardViewController>[0]
-		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
 		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
+		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
 		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 	}
 
