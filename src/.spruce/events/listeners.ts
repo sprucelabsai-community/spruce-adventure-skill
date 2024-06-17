2532d26e6173e634d625f75d9b9c7ebff36324ce
Taylor Romero <me@taylorrome.ro>
2024-06-17 10:38:47 -0600

    patch: versions updated

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -23,13 +23,6 @@ const listeners: EventFeatureListener[] = [
         isGlobal: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').isGlobal,
     },
     {
-        eventName: 'list',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
-        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
-    },
-    {
         eventName: 'accept-connection',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
@@ -51,11 +44,11 @@ const listeners: EventFeatureListener[] = [
         isGlobal: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').isGlobal,
     },
     {
-        eventName: 'register-dashboard-cards',
-        eventNamespace: 'people',
-        version: 'v2022_05_29',
-        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
-        isGlobal: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').isGlobal,
+        eventName: 'list',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
+        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
     },
     {
         eventName: 'post',
@@ -63,6 +56,13 @@ const listeners: EventFeatureListener[] = [
         version: 'v2022_09_09',
         callback: require('../../posting/listeners/adventure/post.v2022_09_09.listener').default,
         isGlobal: require('../../posting/listeners/adventure/post.v2022_09_09.listener').isGlobal,
+    },
+    {
+        eventName: 'register-dashboard-cards',
+        eventNamespace: 'people',
+        version: 'v2022_05_29',
+        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
+        isGlobal: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').isGlobal,
     },
     {
         eventName: 'rsvp',
