a3226c4f07bde5a1f5e2ae48ad3b96831c7b17a9
CircleCI <devops@sprucelabs.ai>
2024-06-19 08:58:13 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -1,33 +1,33 @@
-import ListSkillViewController from '../../listing/List.svc'
 import ConnectSkillViewController from '../../friends/Connect.svc'
-import PostSkillViewController from '../../posting/Post.svc'
+import ListSkillViewController from '../../listing/List.svc'
 import EquipSkillViewController from '../../root/Equip.svc'
 import ProfileSkillViewController from '../../root/Profile.svc'
 import RootSkillViewController from '../../root/Root.svc'
-import FriendsListToolViewController from '../../friends/FriendsListTool.vc'
+import PostSkillViewController from '../../posting/Post.svc'
 import ConfirmCancelCardViewController from '../../cancelling/ConfirmCancelCard.vc'
+import FriendsListToolViewController from '../../friends/FriendsListTool.vc'
 import AdventureCardViewController from '../../listing/AdventureCard.vc'
 import BaseAdventureCardViewController from '../../listing/BaseAdventureCard.vc'
 import CurrentAdventureCardViewController from '../../listing/CurrentAdventureCard.vc'
-import PostCardViewController from '../../posting/PostCard.vc'
 import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
+import PostCardViewController from '../../posting/PostCard.vc'
 
 import '@sprucelabs/heartwood-view-controllers'
 
 const vcs = {
-    ListSkillViewController,
     ConnectSkillViewController,
-    PostSkillViewController,
+    ListSkillViewController,
     EquipSkillViewController,
     ProfileSkillViewController,
     RootSkillViewController,
-    FriendsListToolViewController,
+    PostSkillViewController,
     ConfirmCancelCardViewController,
+    FriendsListToolViewController,
     AdventureCardViewController,
     BaseAdventureCardViewController,
     CurrentAdventureCardViewController,
-    PostCardViewController,
     FriendsDashboardCardViewController,
+    PostCardViewController,
 }
 
 export const pluginsByName = {
@@ -37,47 +37,47 @@ type LoadOptions<Args extends Record<string,any>[]> = Args[0]['args'] extends Re
 
 declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.types' {
 	interface SkillViewControllerMap {
-		'adventure.list': ListSkillViewController
 		'adventure.connect': ConnectSkillViewController
-		'adventure.post': PostSkillViewController
+		'adventure.list': ListSkillViewController
 		'adventure.equip': EquipSkillViewController
 		'adventure.profile': ProfileSkillViewController
 		'adventure.root': RootSkillViewController
+		'adventure.post': PostSkillViewController
 	}
 
 	interface SkillViewControllerArgsMap {
-		'adventure.list': LoadOptions<Parameters<ListSkillViewController['load']>>
 		'adventure.connect': LoadOptions<Parameters<ConnectSkillViewController['load']>>
-		'adventure.post': LoadOptions<Parameters<PostSkillViewController['load']>>
+		'adventure.list': LoadOptions<Parameters<ListSkillViewController['load']>>
 		'adventure.equip': LoadOptions<Parameters<EquipSkillViewController['load']>>
 		'adventure.profile': LoadOptions<Parameters<ProfileSkillViewController['load']>>
 		'adventure.root': LoadOptions<Parameters<RootSkillViewController['load']>>
+		'adventure.post': LoadOptions<Parameters<PostSkillViewController['load']>>
 	}
 
 	interface ViewControllerMap {
-		'adventure.friends-list-tool': FriendsListToolViewController
 		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
+		'adventure.friends-list-tool': FriendsListToolViewController
 		'adventure.adventure-card': AdventureCardViewController
 		'adventure.base-adventure-card': BaseAdventureCardViewController
 		'adventure.current-adventure-card': CurrentAdventureCardViewController
-		'adventure.post-card': PostCardViewController
 		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
-		'adventure.list': ListSkillViewController
+		'adventure.post-card': PostCardViewController
 		'adventure.connect': ConnectSkillViewController
-		'adventure.post': PostSkillViewController
+		'adventure.list': ListSkillViewController
 		'adventure.equip': EquipSkillViewController
 		'adventure.profile': ProfileSkillViewController
 		'adventure.root': RootSkillViewController
+		'adventure.post': PostSkillViewController
 	}
 
     interface ViewControllerOptionsMap {
-		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
 		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
+		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
 		'adventure.adventure-card': ConstructorParameters<typeof AdventureCardViewController>[0]
 		'adventure.base-adventure-card': ConstructorParameters<typeof BaseAdventureCardViewController>[0]
 		'adventure.current-adventure-card': ConstructorParameters<typeof CurrentAdventureCardViewController>[0]
-		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
+		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 	}
 
 	interface ViewControllerPlugins {
