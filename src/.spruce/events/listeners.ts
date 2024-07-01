50c84ab46fe21a1ed82980c4745f8d2ebcebfbfc
CircleCI <devops@sprucelabs.ai>
2024-07-01 12:25:06 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -16,6 +16,20 @@ const listeners: EventFeatureListener[] = [
         isGlobal: require('../../listeners/skill/will-boot.v2021_08_03.listener').isGlobal,
     },
     {
+        eventName: 'cancel',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').default,
+        isGlobal: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').isGlobal,
+    },
+    {
+        eventName: 'list',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
+        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
+    },
+    {
         eventName: 'accept-connection',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
@@ -35,20 +49,6 @@ const listeners: EventFeatureListener[] = [
         version: 'v2022_09_09',
         callback: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').default,
         isGlobal: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').isGlobal,
-    },
-    {
-        eventName: 'cancel',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').default,
-        isGlobal: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').isGlobal,
-    },
-    {
-        eventName: 'list',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
-        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
     },
     {
         eventName: 'post',
