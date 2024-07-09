dc3f58979d38d5dec3ff2a19c4079fd3b79c728a
CircleCI <devops@sprucelabs.ai>
2024-07-09 13:51:33 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -6,11 +6,11 @@ import ProfileSkillViewController from '../../root/Profile.svc'
 import RootSkillViewController from '../../root/Root.svc'
 import ConfirmCancelCardViewController from '../../cancelling/ConfirmCancelCard.vc'
 import FriendsListToolViewController from '../../friends/FriendsListTool.vc'
+import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
+import PostCardViewController from '../../posting/PostCard.vc'
 import AdventureCardViewController from '../../listing/AdventureCard.vc'
 import BaseAdventureCardViewController from '../../listing/BaseAdventureCard.vc'
 import CurrentAdventureCardViewController from '../../listing/CurrentAdventureCard.vc'
-import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
-import PostCardViewController from '../../posting/PostCard.vc'
 
 import '@sprucelabs/heartwood-view-controllers'
 
@@ -23,11 +23,11 @@ const vcs = {
     RootSkillViewController,
     ConfirmCancelCardViewController,
     FriendsListToolViewController,
+    FriendsDashboardCardViewController,
+    PostCardViewController,
     AdventureCardViewController,
     BaseAdventureCardViewController,
     CurrentAdventureCardViewController,
-    FriendsDashboardCardViewController,
-    PostCardViewController,
 }
 
 export const pluginsByName = {
@@ -57,11 +57,11 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 	interface ViewControllerMap {
 		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
 		'adventure.friends-list-tool': FriendsListToolViewController
+		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
+		'adventure.post-card': PostCardViewController
 		'adventure.adventure-card': AdventureCardViewController
 		'adventure.base-adventure-card': BaseAdventureCardViewController
 		'adventure.current-adventure-card': CurrentAdventureCardViewController
-		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
-		'adventure.post-card': PostCardViewController
 		'adventure.connect': ConnectSkillViewController
 		'adventure.list': ListSkillViewController
 		'adventure.post': PostSkillViewController
@@ -73,11 +73,11 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
     interface ViewControllerOptionsMap {
 		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
 		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
+		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
+		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 		'adventure.adventure-card': ConstructorParameters<typeof AdventureCardViewController>[0]
 		'adventure.base-adventure-card': ConstructorParameters<typeof BaseAdventureCardViewController>[0]
 		'adventure.current-adventure-card': ConstructorParameters<typeof CurrentAdventureCardViewController>[0]
-		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
-		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 	}
 
 	interface ViewControllerPlugins {
