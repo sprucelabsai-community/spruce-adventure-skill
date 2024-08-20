1d2ad0a3484e67a5355369d90ddaf1eeb567af2b
Taylor Romero <me@taylorrome.ro>
2024-08-20 06:31:36 -0600

    patch: versions updated

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -23,13 +23,6 @@ const listeners: EventFeatureListener[] = [
         isGlobal: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').isGlobal,
     },
     {
-        eventName: 'register-dashboard-cards',
-        eventNamespace: 'people',
-        version: 'v2022_05_29',
-        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
-        isGlobal: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').isGlobal,
-    },
-    {
         eventName: 'list',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
@@ -56,6 +49,13 @@ const listeners: EventFeatureListener[] = [
         version: 'v2022_09_09',
         callback: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').default,
         isGlobal: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').isGlobal,
+    },
+    {
+        eventName: 'register-dashboard-cards',
+        eventNamespace: 'people',
+        version: 'v2022_05_29',
+        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
+        isGlobal: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').isGlobal,
     },
     {
         eventName: 'post',
--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -4,13 +4,13 @@ import PostSkillViewController from '../../posting/Post.svc'
 import EquipSkillViewController from '../../root/Equip.svc'
 import ProfileSkillViewController from '../../root/Profile.svc'
 import RootSkillViewController from '../../root/Root.svc'
-import FriendsListToolViewController from '../../friends/FriendsListTool.vc'
 import ConfirmCancelCardViewController from '../../cancelling/ConfirmCancelCard.vc'
+import FriendsListToolViewController from '../../friends/FriendsListTool.vc'
 import AdventureCardViewController from '../../listing/AdventureCard.vc'
 import BaseAdventureCardViewController from '../../listing/BaseAdventureCard.vc'
 import CurrentAdventureCardViewController from '../../listing/CurrentAdventureCard.vc'
-import PostCardViewController from '../../posting/PostCard.vc'
 import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
+import PostCardViewController from '../../posting/PostCard.vc'
 
 import '@sprucelabs/heartwood-view-controllers'
 
@@ -21,13 +21,13 @@ const vcs = {
     EquipSkillViewController,
     ProfileSkillViewController,
     RootSkillViewController,
-    FriendsListToolViewController,
     ConfirmCancelCardViewController,
+    FriendsListToolViewController,
     AdventureCardViewController,
     BaseAdventureCardViewController,
     CurrentAdventureCardViewController,
-    PostCardViewController,
     FriendsDashboardCardViewController,
+    PostCardViewController,
 }
 
 export const pluginsByName = {
@@ -55,13 +55,13 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 	}
 
 	interface ViewControllerMap {
-		'adventure.friends-list-tool': FriendsListToolViewController
 		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
+		'adventure.friends-list-tool': FriendsListToolViewController
 		'adventure.adventure-card': AdventureCardViewController
 		'adventure.base-adventure-card': BaseAdventureCardViewController
 		'adventure.current-adventure-card': CurrentAdventureCardViewController
-		'adventure.post-card': PostCardViewController
 		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
+		'adventure.post-card': PostCardViewController
 		'adventure.connect': ConnectSkillViewController
 		'adventure.list': ListSkillViewController
 		'adventure.post': PostSkillViewController
@@ -71,13 +71,13 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 	}
 
     interface ViewControllerOptionsMap {
-		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
 		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
+		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
 		'adventure.adventure-card': ConstructorParameters<typeof AdventureCardViewController>[0]
 		'adventure.base-adventure-card': ConstructorParameters<typeof BaseAdventureCardViewController>[0]
 		'adventure.current-adventure-card': ConstructorParameters<typeof CurrentAdventureCardViewController>[0]
-		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
+		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 	}
 
 	interface ViewControllerPlugins {
