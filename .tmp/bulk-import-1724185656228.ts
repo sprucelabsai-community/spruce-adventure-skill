32da845b884dbc51c98f6d05e75e742d5b73f82c
Taylor Romero <me@taylorrome.ro>
2024-08-20 14:27:36 -0600

    patch: upgrade

---
+++ .tmp/bulk-import-1724185656228.ts
@@ -0,0 +1,88 @@
+import { default as import0} from '../src/cancelling/events/cancel/v2022_09_09/emitPayload.builder'
+import { default as import1} from '../src/cancelling/events/cancel/v2022_09_09/emitPermissions.builder'
+import { default as import2} from '../src/cancelling/events/cancel/v2022_09_09/emitTarget.builder'
+import { default as import3} from '../src/cancelling/events/cancel/v2022_09_09/event.options'
+import { default as import4} from '../src/cancelling/events/cancel/v2022_09_09/listenPermissions.builder'
+import { default as import5} from '../src/cancelling/events/cancel/v2022_09_09/responsePayload.builder'
+import { default as import6} from '../src/friends/events/create-connection/v2022_09_09/emitPayload.builder'
+import { default as import7} from '../src/friends/events/create-connection/v2022_09_09/emitPermissions.builder'
+import { default as import8} from '../src/friends/events/create-connection/v2022_09_09/emitTarget.builder'
+import { default as import9} from '../src/friends/events/create-connection/v2022_09_09/event.options'
+import { default as import10} from '../src/friends/events/create-connection/v2022_09_09/listenPermissions.builder'
+import { default as import11} from '../src/friends/events/create-connection/v2022_09_09/responsePayload.builder'
+import { default as import12} from '../src/friends/events/accept-connection/v2022_09_09/emitPayload.builder'
+import { default as import13} from '../src/friends/events/accept-connection/v2022_09_09/emitPermissions.builder'
+import { default as import14} from '../src/friends/events/accept-connection/v2022_09_09/emitTarget.builder'
+import { default as import15} from '../src/friends/events/accept-connection/v2022_09_09/event.options'
+import { default as import16} from '../src/friends/events/accept-connection/v2022_09_09/listenPermissions.builder'
+import { default as import17} from '../src/friends/events/accept-connection/v2022_09_09/responsePayload.builder'
+import { default as import18} from '../src/listing/events/list/v2022_09_09/emitPayload.builder'
+import { default as import19} from '../src/listing/events/list/v2022_09_09/emitPermissions.builder'
+import { default as import20} from '../src/listing/events/list/v2022_09_09/emitTarget.builder'
+import { default as import21} from '../src/listing/events/list/v2022_09_09/event.options'
+import { default as import22} from '../src/listing/events/list/v2022_09_09/listenPermissions.builder'
+import { default as import23} from '../src/listing/events/list/v2022_09_09/responsePayload.builder'
+import { default as import24} from '../src/rsvping/events/rsvp/v2022_09_09/emitPayload.builder'
+import { default as import25} from '../src/rsvping/events/rsvp/v2022_09_09/emitPermissions.builder'
+import { default as import26} from '../src/rsvping/events/rsvp/v2022_09_09/emitTarget.builder'
+import { default as import27} from '../src/rsvping/events/rsvp/v2022_09_09/event.options'
+import { default as import28} from '../src/rsvping/events/rsvp/v2022_09_09/listenPermissions.builder'
+import { default as import29} from '../src/rsvping/events/rsvp/v2022_09_09/responsePayload.builder'
+import { default as import30} from '../src/friends/events/list-friends/v2022_09_09/emitPayload.builder'
+import { default as import31} from '../src/friends/events/list-friends/v2022_09_09/emitPermissions.builder'
+import { default as import32} from '../src/friends/events/list-friends/v2022_09_09/emitTarget.builder'
+import { default as import33} from '../src/friends/events/list-friends/v2022_09_09/event.options'
+import { default as import34} from '../src/friends/events/list-friends/v2022_09_09/listenPermissions.builder'
+import { default as import35} from '../src/friends/events/list-friends/v2022_09_09/responsePayload.builder'
+import { default as import36} from '../src/posting/events/post/v2022_09_09/emitPayload.builder'
+import { default as import37} from '../src/posting/events/post/v2022_09_09/emitPermissions.builder'
+import { default as import38} from '../src/posting/events/post/v2022_09_09/emitTarget.builder'
+import { default as import39} from '../src/posting/events/post/v2022_09_09/event.options'
+import { default as import40} from '../src/posting/events/post/v2022_09_09/listenPermissions.builder'
+import { default as import41} from '../src/posting/events/post/v2022_09_09/responsePayload.builder'
+
+
+export default [
+import0,
+import1,
+import2,
+import3,
+import4,
+import5,
+import6,
+import7,
+import8,
+import9,
+import10,
+import11,
+import12,
+import13,
+import14,
+import15,
+import16,
+import17,
+import18,
+import19,
+import20,
+import21,
+import22,
+import23,
+import24,
+import25,
+import26,
+import27,
+import28,
+import29,
+import30,
+import31,
+import32,
+import33,
+import34,
+import35,
+import36,
+import37,
+import38,
+import39,
+import40,
+import41,
+]
