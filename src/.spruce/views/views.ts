8ee000420c13c832f6dbeed45fde67f5bd0ca273
CircleCI <devops@sprucelabs.ai>
2024-09-02 17:28:12 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -1,11 +1,11 @@
-import PostSkillViewController from '../../posting/Post.svc'
 import ConnectSkillViewController from '../../friends/Connect.svc'
 import ListSkillViewController from '../../listing/List.svc'
+import PostSkillViewController from '../../posting/Post.svc'
 import EquipSkillViewController from '../../root/Equip.svc'
 import ProfileSkillViewController from '../../root/Profile.svc'
 import RootSkillViewController from '../../root/Root.svc'
-import ConfirmCancelCardViewController from '../../cancelling/ConfirmCancelCard.vc'
 import FriendsListToolViewController from '../../friends/FriendsListTool.vc'
+import ConfirmCancelCardViewController from '../../cancelling/ConfirmCancelCard.vc'
 import AdventureCardViewController from '../../listing/AdventureCard.vc'
 import BaseAdventureCardViewController from '../../listing/BaseAdventureCard.vc'
 import CurrentAdventureCardViewController from '../../listing/CurrentAdventureCard.vc'
@@ -15,14 +15,14 @@ import PostCardViewController from '../../posting/PostCard.vc'
 import '@sprucelabs/heartwood-view-controllers'
 
 const vcs = {
-    PostSkillViewController,
     ConnectSkillViewController,
     ListSkillViewController,
+    PostSkillViewController,
     EquipSkillViewController,
     ProfileSkillViewController,
     RootSkillViewController,
-    ConfirmCancelCardViewController,
     FriendsListToolViewController,
+    ConfirmCancelCardViewController,
     AdventureCardViewController,
     BaseAdventureCardViewController,
     CurrentAdventureCardViewController,
@@ -37,42 +37,42 @@ type LoadOptions<Args extends Record<string,any>[]> = Args[0]['args'] extends Re
 
 declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.types' {
 	interface SkillViewControllerMap {
-		'adventure.post': PostSkillViewController
 		'adventure.connect': ConnectSkillViewController
 		'adventure.list': ListSkillViewController
+		'adventure.post': PostSkillViewController
 		'adventure.equip': EquipSkillViewController
 		'adventure.profile': ProfileSkillViewController
 		'adventure.root': RootSkillViewController
 	}
 
 	interface SkillViewControllerArgsMap {
-		'adventure.post': LoadOptions<Parameters<PostSkillViewController['load']>>
 		'adventure.connect': LoadOptions<Parameters<ConnectSkillViewController['load']>>
 		'adventure.list': LoadOptions<Parameters<ListSkillViewController['load']>>
+		'adventure.post': LoadOptions<Parameters<PostSkillViewController['load']>>
 		'adventure.equip': LoadOptions<Parameters<EquipSkillViewController['load']>>
 		'adventure.profile': LoadOptions<Parameters<ProfileSkillViewController['load']>>
 		'adventure.root': LoadOptions<Parameters<RootSkillViewController['load']>>
 	}
 
 	interface ViewControllerMap {
-		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
 		'adventure.friends-list-tool': FriendsListToolViewController
+		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
 		'adventure.adventure-card': AdventureCardViewController
 		'adventure.base-adventure-card': BaseAdventureCardViewController
 		'adventure.current-adventure-card': CurrentAdventureCardViewController
 		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
 		'adventure.post-card': PostCardViewController
-		'adventure.post': PostSkillViewController
 		'adventure.connect': ConnectSkillViewController
 		'adventure.list': ListSkillViewController
+		'adventure.post': PostSkillViewController
 		'adventure.equip': EquipSkillViewController
 		'adventure.profile': ProfileSkillViewController
 		'adventure.root': RootSkillViewController
 	}
 
     interface ViewControllerOptionsMap {
-		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
 		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
+		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
 		'adventure.adventure-card': ConstructorParameters<typeof AdventureCardViewController>[0]
 		'adventure.base-adventure-card': ConstructorParameters<typeof BaseAdventureCardViewController>[0]
 		'adventure.current-adventure-card': ConstructorParameters<typeof CurrentAdventureCardViewController>[0]
