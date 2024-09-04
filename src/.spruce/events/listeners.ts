6c24ce2b0aa782ce2025dae072553ef4233beb02
Taylor Romero <me@taylorrome.ro>
2024-09-03 23:23:12 -0600

    patch: versions updated

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -23,20 +23,6 @@ const listeners: EventFeatureListener[] = [
         isGlobal: require('../../cancelling/listeners/adventure/cancel.v2022_09_09.listener').isGlobal,
     },
     {
-        eventName: 'list',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
-        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
-    },
-    {
-        eventName: 'register-dashboard-cards',
-        eventNamespace: 'people',
-        version: 'v2022_05_29',
-        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
-        isGlobal: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').isGlobal,
-    },
-    {
         eventName: 'accept-connection',
         eventNamespace: 'adventure',
         version: 'v2022_09_09',
@@ -56,6 +42,20 @@ const listeners: EventFeatureListener[] = [
         version: 'v2022_09_09',
         callback: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').default,
         isGlobal: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').isGlobal,
+    },
+    {
+        eventName: 'register-dashboard-cards',
+        eventNamespace: 'people',
+        version: 'v2022_05_29',
+        callback: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').default,
+        isGlobal: require('../../peopleDashboardCards/listeners/people/register-dashboard-cards.v2022_05_29.listener').isGlobal,
+    },
+    {
+        eventName: 'list',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
+        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
     },
     {
         eventName: 'post',
