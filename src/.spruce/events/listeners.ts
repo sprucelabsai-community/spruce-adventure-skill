49e3c79e36d3895de82f96deb15034d874cf16ea
CircleCI <devops@sprucelabs.ai>
2024-09-05 19:42:49 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -44,6 +44,13 @@ const listeners: EventFeatureListener[] = [
         isGlobal: require('../../friends/listeners/adventure/list-friends.v2022_09_09.listener').isGlobal,
     },
     {
+        eventName: 'list',
+        eventNamespace: 'adventure',
+        version: 'v2022_09_09',
+        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
+        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
+    },
+    {
         eventName: 'register-dashboard-cards',
         eventNamespace: 'people',
         version: 'v2022_05_29',
@@ -63,13 +70,6 @@ const listeners: EventFeatureListener[] = [
         version: 'v2022_09_09',
         callback: require('../../rsvping/listeners/adventure/rsvp.v2022_09_09.listener').default,
         isGlobal: require('../../rsvping/listeners/adventure/rsvp.v2022_09_09.listener').isGlobal,
-    },
-    {
-        eventName: 'list',
-        eventNamespace: 'adventure',
-        version: 'v2022_09_09',
-        callback: require('../../listing/listeners/adventure/list.v2022_09_09.listener').default,
-        isGlobal: require('../../listing/listeners/adventure/list.v2022_09_09.listener').isGlobal,
     },
 ]
 
