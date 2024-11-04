bcd30a9086149ee123aea617bf91c34c21dcda3e
Taylor Romero <me@taylorrome.ro>
2024-11-04 13:56:29 -0700

    patch: upgrade

--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -6,13 +6,13 @@ import ConnectSkillViewController from '../../friends/connecting/Connect.svc'
 import FriendSelectionCardViewController from '../../groups/FriendSelectionCard.vc'
 import GroupFormCardViewController from '../../groups/GroupFormCard.vc'
 import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
+import FriendsListToolViewController from '../../friends/listing/FriendsListTool.vc'
+import GroupListCardViewController from '../../groups/listing/GroupListCard.vc'
 import ConfirmCancelCardViewController from '../../adventures/cancelling/ConfirmCancelCard.vc'
 import AdventureCardViewController from '../../adventures/listing/AdventureCard.vc'
 import BaseAdventureCardViewController from '../../adventures/listing/BaseAdventureCard.vc'
 import CurrentAdventureCardViewController from '../../adventures/listing/CurrentAdventureCard.vc'
 import PostCardViewController from '../../adventures/posting/PostCard.vc'
-import FriendsListToolViewController from '../../friends/listing/FriendsListTool.vc'
-import GroupListCardViewController from '../../groups/listing/GroupListCard.vc'
 
 import '@sprucelabs/heartwood-view-controllers'
 
@@ -25,13 +25,13 @@ const vcs = {
     FriendSelectionCardViewController,
     GroupFormCardViewController,
     FriendsDashboardCardViewController,
+    FriendsListToolViewController,
+    GroupListCardViewController,
     ConfirmCancelCardViewController,
     AdventureCardViewController,
     BaseAdventureCardViewController,
     CurrentAdventureCardViewController,
     PostCardViewController,
-    FriendsListToolViewController,
-    GroupListCardViewController,
 }
 
 export const pluginsByName = {
@@ -60,13 +60,13 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 		'adventure.friend-selection-card': FriendSelectionCardViewController
 		'adventure.group-form-card': GroupFormCardViewController
 		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
+		'adventure.friends-list-tool': FriendsListToolViewController
+		'adventure.group-list-card': GroupListCardViewController
 		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
 		'adventure.adventure-card': AdventureCardViewController
 		'adventure.base-adventure-card': BaseAdventureCardViewController
 		'adventure.current-adventure-card': CurrentAdventureCardViewController
 		'adventure.post-card': PostCardViewController
-		'adventure.friends-list-tool': FriendsListToolViewController
-		'adventure.group-list-card': GroupListCardViewController
 		'adventure.group': GroupSkillViewController
 		'adventure.root': RootSkillViewController
 		'adventure.list': ListSkillViewController
@@ -78,13 +78,13 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 		'adventure.friend-selection-card': ConstructorParameters<typeof FriendSelectionCardViewController>[0]
 		'adventure.group-form-card': ConstructorParameters<typeof GroupFormCardViewController>[0]
 		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
+		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
+		'adventure.group-list-card': ConstructorParameters<typeof GroupListCardViewController>[0]
 		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
 		'adventure.adventure-card': ConstructorParameters<typeof AdventureCardViewController>[0]
 		'adventure.base-adventure-card': ConstructorParameters<typeof BaseAdventureCardViewController>[0]
 		'adventure.current-adventure-card': ConstructorParameters<typeof CurrentAdventureCardViewController>[0]
 		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
-		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
-		'adventure.group-list-card': ConstructorParameters<typeof GroupListCardViewController>[0]
 	}
 
 	interface ViewControllerPlugins {
