b11ebef4e8c1e79a5591a8a0e1f60a3b33bfd41c
CircleCI <devops@sprucelabs.ai>
2025-04-24 05:49:12 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -1,15 +1,15 @@
-import GroupSkillViewController from '../../groups/Group.svc'
 import RootSkillViewController from '../../root/Root.svc'
-import ListSkillViewController from '../../adventures/listing/List.svc'
+import GroupSkillViewController from '../../groups/Group.svc'
 import PostSkillViewController from '../../adventures/posting/Post.svc'
+import ListSkillViewController from '../../adventures/listing/List.svc'
 import ConnectSkillViewController from '../../friends/connecting/Connect.svc'
 import FriendSelectionCardViewController from '../../groups/FriendSelectionCard.vc'
 import GroupFormCardViewController from '../../groups/GroupFormCard.vc'
 import FriendsDashboardCardViewController from '../../peopleDashboardCards/FriendsDashboardCard.vc'
-import ConfirmCancelCardViewController from '../../adventures/cancelling/ConfirmCancelCard.vc'
 import AdventureCardViewController from '../../adventures/listing/AdventureCard.vc'
 import BaseAdventureCardViewController from '../../adventures/listing/BaseAdventureCard.vc'
 import CurrentAdventureCardViewController from '../../adventures/listing/CurrentAdventureCard.vc'
+import ConfirmCancelCardViewController from '../../adventures/cancelling/ConfirmCancelCard.vc'
 import PostCardViewController from '../../adventures/posting/PostCard.vc'
 import GroupListCardViewController from '../../groups/listing/GroupListCard.vc'
 import FriendsListToolViewController from '../../friends/listing/FriendsListTool.vc'
@@ -17,18 +17,18 @@ import FriendsListToolViewController from '../../friends/listing/FriendsListTool
 import '@sprucelabs/heartwood-view-controllers'
 
 const vcs = {
-    GroupSkillViewController,
     RootSkillViewController,
-    ListSkillViewController,
+    GroupSkillViewController,
     PostSkillViewController,
+    ListSkillViewController,
     ConnectSkillViewController,
     FriendSelectionCardViewController,
     GroupFormCardViewController,
     FriendsDashboardCardViewController,
-    ConfirmCancelCardViewController,
     AdventureCardViewController,
     BaseAdventureCardViewController,
     CurrentAdventureCardViewController,
+    ConfirmCancelCardViewController,
     PostCardViewController,
     GroupListCardViewController,
     FriendsListToolViewController,
@@ -43,18 +43,18 @@ type LoadOptions<Args extends Record<string,any>[]> = Args[0]['args'] extends Re
 
 declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.types' {
 	interface SkillViewControllerMap {
-		'adventure.group': GroupSkillViewController
 		'adventure.root': RootSkillViewController
-		'adventure.list': ListSkillViewController
+		'adventure.group': GroupSkillViewController
 		'adventure.post': PostSkillViewController
+		'adventure.list': ListSkillViewController
 		'adventure.connect': ConnectSkillViewController
 	}
 
 	interface SkillViewControllerArgsMap {
-		'adventure.group': LoadOptions<Parameters<GroupSkillViewController['load']>>
 		'adventure.root': LoadOptions<Parameters<RootSkillViewController['load']>>
-		'adventure.list': LoadOptions<Parameters<ListSkillViewController['load']>>
+		'adventure.group': LoadOptions<Parameters<GroupSkillViewController['load']>>
 		'adventure.post': LoadOptions<Parameters<PostSkillViewController['load']>>
+		'adventure.list': LoadOptions<Parameters<ListSkillViewController['load']>>
 		'adventure.connect': LoadOptions<Parameters<ConnectSkillViewController['load']>>
 	}
 
@@ -62,17 +62,17 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 		'adventure.friend-selection-card': FriendSelectionCardViewController
 		'adventure.group-form-card': GroupFormCardViewController
 		'adventure.friends-dashboard-card': FriendsDashboardCardViewController
-		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
 		'adventure.adventure-card': AdventureCardViewController
 		'adventure.base-adventure-card': BaseAdventureCardViewController
 		'adventure.current-adventure-card': CurrentAdventureCardViewController
+		'adventure.confirm-cancel-card': ConfirmCancelCardViewController
 		'adventure.post-card': PostCardViewController
 		'adventure.group-list-card': GroupListCardViewController
 		'adventure.friends-list-tool': FriendsListToolViewController
-		'adventure.group': GroupSkillViewController
 		'adventure.root': RootSkillViewController
-		'adventure.list': ListSkillViewController
+		'adventure.group': GroupSkillViewController
 		'adventure.post': PostSkillViewController
+		'adventure.list': ListSkillViewController
 		'adventure.connect': ConnectSkillViewController
 	}
 
@@ -80,10 +80,10 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 		'adventure.friend-selection-card': ConstructorParameters<typeof FriendSelectionCardViewController>[0]
 		'adventure.group-form-card': ConstructorParameters<typeof GroupFormCardViewController>[0]
 		'adventure.friends-dashboard-card': ConstructorParameters<typeof FriendsDashboardCardViewController>[0]
-		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
 		'adventure.adventure-card': ConstructorParameters<typeof AdventureCardViewController>[0]
 		'adventure.base-adventure-card': ConstructorParameters<typeof BaseAdventureCardViewController>[0]
 		'adventure.current-adventure-card': ConstructorParameters<typeof CurrentAdventureCardViewController>[0]
+		'adventure.confirm-cancel-card': ConstructorParameters<typeof ConfirmCancelCardViewController>[0]
 		'adventure.post-card': ConstructorParameters<typeof PostCardViewController>[0]
 		'adventure.group-list-card': ConstructorParameters<typeof GroupListCardViewController>[0]
 		'adventure.friends-list-tool': ConstructorParameters<typeof FriendsListToolViewController>[0]
