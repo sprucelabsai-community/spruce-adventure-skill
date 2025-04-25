c22852981b4376722c3ebec8eba97e62bee62464
CircleCI <devops@sprucelabs.ai>
2025-04-25 09:20:20 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/stores/stores.types.ts
+++ src/.spruce/stores/stores.types.ts
@@ -1,17 +1,17 @@
-import AdventuresStore from '../../adventures/Adventures.store'
 import GroupsStore from '../../groups/Groups.store'
+import AdventuresStore from '../../adventures/Adventures.store'
 import ConnectionsStore from '../../friends/connecting/Connections.store'
 
 declare module '@sprucelabs/data-stores/build/types/stores.types' {
 	interface StoreMap {
-                adventures: AdventuresStore
                 groups: GroupsStore
+                adventures: AdventuresStore
                 connections: ConnectionsStore
 	}
 
 	interface StoreOptionsMap {
-                adventures: Omit<Parameters<typeof AdventuresStore['Store']>[0], keyof UniversalStoreOptions>   
                 groups: Omit<Parameters<typeof GroupsStore['Store']>[0], keyof UniversalStoreOptions>   
+                adventures: Omit<Parameters<typeof AdventuresStore['Store']>[0], keyof UniversalStoreOptions>   
                 connections: Omit<Parameters<typeof ConnectionsStore['Store']>[0], keyof UniversalStoreOptions>   
         }
 }
